3baa63fb524143aa1a3c6882a971ca2da777ae45
goeun441 <20200298@dongduk.ac.kr>
2024-03-25 11:47:00 +0900

    모바일소프트웨어 4주차 2교시 수업(위젯까지)

---
+++ week04/app/src/main/java/ddwu/mobile/week04/MainActivity.kt
@@ -0,0 +1,15 @@
+package ddwu.mobile.week04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    /*    val textView : TextView = TextView(this)
+        textView.setText("코드로 문자열 출력")
+        setContentView((textView))*/
+    }
+}
