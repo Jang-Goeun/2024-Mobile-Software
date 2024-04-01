4581db344a1aeee4643352ef3c57b7b75eea44dc
goeun441 <20200298@dongduk.ac.kr>
2024-04-01 11:45:01 +0900

    모바일소프트웨어 5주차 2교시 수업(레이아웃)

---
+++ week05/app/src/main/java/ddwu/mobile/week05/MainActivity.kt
@@ -0,0 +1,33 @@
+package ddwu.mobile.week05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import ddwu.mobile.week05.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.linear_layout)
+
+        val mainBinding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        // val newBinding : NewLayoutBinding = NewLayoutBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+
+
+        val button : Button = findViewById(R.id.button2)
+
+        button.setOnClickListener {
+            val layout : LinearLayout = findViewById(R.id.linear_layout)
+            layout.orientation = LinearLayout.VERTICAL
+        }
+    }
+
+//    fun onClick(view: View){
+//        val layout : LinearLayout = findViewById(R.id.linear_layout)
+//        layout.orientation = LinearLayout.VERTICAL
+//    }
+}
