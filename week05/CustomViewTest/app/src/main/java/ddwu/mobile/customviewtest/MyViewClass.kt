ac49764637a77e15364aa2a60e9b36d0ff146845
goeun441 <20200298@dongduk.ac.kr>
2024-04-01 13:13:32 +0900

    모바일소프트웨어 5주차 3교시 수업

---
+++ week05/CustomViewTest/app/src/main/java/ddwu/mobile/customviewtest/MyViewClass.kt
@@ -0,0 +1,26 @@
+package ddwu.mobile.customviewtest
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class MyViewClass : View {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr:Int)
+            : super(context, attrs, defStyleAttr)
+
+    var color = Color.BLUE
+    override fun onDraw(canvas: Canvas){
+        super.onDraw(canvas)
+        canvas?.drawColor(Color.LTGRAY)
+        val paint = Paint()
+        paint.setColor(color)
+        canvas?.drawCircle(200.toFloat(), 200.toFloat(), 100.toFloat(), paint)
+    }
+}
