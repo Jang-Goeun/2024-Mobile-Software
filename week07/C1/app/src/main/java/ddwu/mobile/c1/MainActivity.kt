b2990bf93b69ad361628150c96bed6f782e8f4a4
goeun441 <20200298@dongduk.ac.kr>
2024-04-15 13:04:07 +0900

    모바일소프트웨어 07주차 수업 3교시

--- week07/C1/app/src/main/java/ddwu/mobile/c1/MainActivity.kt
+++ week07/C1/app/src/main/java/ddwu/mobile/c1/MainActivity.kt
@@ -2,6 +2,7 @@ package ddwu.mobile.c1
 
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
@@ -9,38 +10,81 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ddwu.mobile.c1.databinding.ActivityMainBinding
+import ddwu.mobile.c1.databinding.DialogInterfaceBinding
 
 class MainActivity : AppCompatActivity() {
     val mainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    // var selectedIdx = 0    라디오 버튼
+    //var selectedItems = booleanArrayOf(false, false, false, false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(mainBinding.root)
+
+        mainBinding.button.setOnClickListener{
+            val dialogBinding = DialogInterfaceBinding.inflate(layoutInflater)
+
+            val builder = AlertDialog.Builder(this).apply {
+                setTitle("대화상자 제목")
+                setView(dialogBinding.root)
+                setIcon(R.mipmap.ic_launcher_round)
+                setNegativeButton("취소", null)
+                setCancelable(true)
+            }
+
+            builder.show()
+        }
+    }
+}
+
+
+
 
 //        val onOnClick = object :DialogInterface.OnClickListener {
 //            override fun onClick(p0: DialogInterface?, whichButton: Int) {
 //                Toast.makeText(this@MainActivity, "확인!", Toast.LENGTH_SHORT).show()
 //            }
 //        }
+//
+//        val onOnClick = DialogInterface.OnClickListener {
+//            dialogInerface : DialogInterface?, whichButton: Int ->
+//                Toast.makeText(this@MainActivity, "확인!", Toast.LENGTH_SHORT).show()
+//        }
 
-        val onOnClick = DialogInterface.OnClickListener {
-            dialogInerface : DialogInterface?, whichButton: Int ->
-                Toast.makeText(this@MainActivity, "확인!", Toast.LENGTH_SHORT).show()
-        }
+//setMessage("대화상자 메시지")
+//setItems(R.array.foods) {
+//    dialogInterface : DialogInterface?, idx: Int ->
+//        val food = resources.getStringArray(R.array.foods)
+//        Toast.makeText(this@MainActivity, "${food[idx]}", Toast.LENGTH_SHORT).show()
+//}  아이템 항목을 선택할 수 있기 때문
 
-        mainBinding.button.setOnClickListener{
-            val builder = AlertDialog.Builder(this).apply {
-                setTitle("대화상자 제목")
-                setMessage("대화상자 메시지")
-                setIcon(R.mipmap.ic_launcher_round)
-                setPositiveButton("확인"){ p0: DialogInterface?, whichButton: Int ->
-                    Toast.makeText(this@MainActivity, "확인", Toast.LENGTH_SHORT).show()
-                }
-                setNegativeButton("취소", null)
-                setCancelable(true)
-            }
+//setSingleChoiceItems(R.array.foods, selectedIdx) {
+//    dialogInterface : DialogInterface?, idx: Int ->
+//        selectedIdx = idx
+//}    라디오버튼
+//setPositiveButton("확인"){
+//       p0: DialogInterface?, whichButton: Int ->
+//    val food = resources.getStringArray(R.array.foods)
+//    Toast.makeText(this@MainActivity, "${food[selectedIdx]}", Toast.LENGTH_SHORT).show()
+//}
+
+//setMultiChoiceItems(R.array.foods, selectedItems){
+//    p0: DialogInterface?, idx: Int, isChecked:Boolean ->
+//        selectedItems[idx] = true
+//}
+
+//setPositiveButton("확인"){
+//    p0: DialogInterface?, idx: Int ->
+//        Log.d("MainActivity","${selectedItems.toList()}")
+//        val food = resources.getStringArray(R.array.foods)
+//        Toast.makeText(this@MainActivity, "${selectedItems.toList()}", Toast.LENGTH_SHORT).show()
+//}
+
+
+
 
 //            val builder : AlertDialog.Builder = AlertDialog.Builder(this)
 //            builder.setTitle("대화상자 제목")
@@ -51,16 +95,5 @@ class MainActivity : AppCompatActivity() {
 //            builder.setNegativeButton("취소", null)
 //            builder.setCancelable(false)
 
-            //val dialog: Dialog = builder.create()
-            // dialog.show()
-            builder.show()
-        }
-    }
-}
-
-
-
-
-
-
-
+//val dialog: Dialog = builder.create()
+// dialog.show()
