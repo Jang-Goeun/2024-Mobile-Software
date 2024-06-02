4703a0a8e0790a70ca6f6549ebdb47a00a23d762
goeun441 <20200298@dongduk.ac.kr>
2024-06-02 15:25:03 +0900

    모바일소프트웨어 13주차 수업 내용

---
+++ week13/FoodDBExam/app/src/main/java/ddwu/com/mobile/fooddbexam/AddActivity.kt
@@ -0,0 +1,39 @@
+package ddwu.com.mobile.fooddbexam
+
+import android.content.ContentValues
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import ddwu.com.mobile.fooddbexam.databinding.ActivityAddBinding
+
+class AddActivity : AppCompatActivity() {
+
+    val addBinding by lazy {
+        ActivityAddBinding.inflate(layoutInflater)
+    }
+
+    lateinit var helper : FoodDBHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(addBinding.root)
+
+        helper = FoodDBHelper(this)
+
+        addBinding.btnAddFood.setOnClickListener{
+            addFood(addBinding.etAddFood.text.toString(), addBinding.etAddCountry.text.toString())
+        }
+    }
+
+
+    fun addFood(food: String, country: String) {
+        val db = helper.writableDatabase
+        val newRow = ContentValues()
+        newRow.put(FoodDBHelper.COL_FOOD, food)
+        newRow.put(FoodDBHelper.COL_COUNTRY, country)
+        db.insert(FoodDBHelper.TABLE_NAME, null, newRow)
+        //db.execSQL("insert into ${FoodDBHelper.TABLE_NAME} values (null, '순두부찌개', '대한민국')")
+        helper.close()
+    }
+
+}
